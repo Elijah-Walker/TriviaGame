0922e81688178e6f7cd67d9471bcd2cc4e040004
Elijah-Walker <Elijah.Walker747@gmail.com>
2018-05-03 22:54:22 -0600

    questions ready

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+
+$('input[name=radioName]:checked').val();
+
+
+
+
+
+// if(jQuery("#b1").data("clicked")) {
+//     console.log("right");
+// } 
+
+// else if (jQuery("#a1").data("clicked")) {
+//     console.log("wrong");
+// }
+
+// else if  (jQuery("#c1").data("clicked")) {
+//     console.log("wrong");
+// }
+
+
+// else if (jQuery("#d1s").data("clicked")) {
+//     console.log("wrong");
+// }
